5979b926318fda7c69df72dfe5d945beaf808bfa
level237 <Bramslevel129@gmail.com>
2024-06-17 21:30:57 +0100

    init connection in mongodb

---
+++ app/signup/page.tsx
@@ -0,0 +1,10 @@
+
+export default function SignupPage(){
+
+    return (
+        <div className="flex">
+        <h1 className="text-center text-white text-2xl">Signup Page</h1>
+        </div>
+    )
+
+}
