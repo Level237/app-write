49d6ae52195dfc6fb12b7b4a176fce60dde91a8c
level237 <Bramslevel129@gmail.com>
2024-06-17 23:20:39 +0100

    signup in action

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,9 +1,9 @@
 "use client"
 import Link from "next/link"
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { useRouter } from "next/navigation"
-import {axios} from "axios"
-
+import axios from "axios"
+import {toast} from "react-hot-toast"
 
 export default function SignupPage(){
 
@@ -15,17 +15,38 @@ export default function SignupPage(){
     })
 
     const [buttonDisable,setButtonDisabled]=React.useState(false)
+    const [loading,setLoading]=useState(false)
     const onSignup=async ()=>{
+        console.log("d")
+        try {
+            setLoading(true)
+            const response=await axios.post('/api/users/signup',user)
 
+            console.log("signup success",response.data)
+            router.push("/login")
+        } catch (error:any) {
+            console.log("Signup failed",error.message)
+            toast.error(error.message)
+        }finally{
+            setLoading(false)
+        }
     }
+
+    useEffect(()=>{
+        if(user.email.length>0 && user.password.length >0 && user.username.length >0){
+            setButtonDisabled(false)
+        }else{
+            setButtonDisabled(true)
+        }
+    },[user])
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
-        <h1 className="text-center text-white text-2xl">Signup Page</h1>
+        <h1 className="text-center text-white text-2xl">{!loading ? "Signup" : "Processing..."}</h1>
         <hr />
         
         <label htmlFor="username">Username</label>
         <input
-        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        className="p-2 border text-black border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
          id="username"
           type="text"
           value={user.username}
@@ -36,7 +57,7 @@ export default function SignupPage(){
       
         <label htmlFor="email">email</label>
         <input
-        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        className="p-2  text-black border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
          id="email"
           type="email"
           value={user.email}
@@ -47,7 +68,7 @@ export default function SignupPage(){
      
         <label htmlFor="password">password</label>
         <input
-        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        className="p-2 border  text-black border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
          id="password"
           type="password"
           value={user.password}
@@ -59,7 +80,7 @@ export default function SignupPage(){
             <button
             onClick={onSignup}
             className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">
-                Signup
+                {buttonDisable ? "No signup" : "Signup"}
             </button>
         </div>
         <Link href="/login">Login</Link>
