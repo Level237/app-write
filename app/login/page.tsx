895de01ec31b4bc7b6f397d32edd06354071652a
level237 <Bramslevel129@gmail.com>
2024-06-18 16:37:42 +0100

    login in action

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,18 +1,39 @@
 "use client"
 import Link from "next/link"
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { useRouter } from "next/navigation"
-import {axios} from "axios"
+import axios from "axios"
+import toast from "react-hot-toast"
 
 
 export default function LoginPage(){
+    const router=useRouter()
     const [user,setUser]=React.useState({
         email:'',
         password:"",
     })
+    const [buttonDisable,setButtonDisabled]=React.useState(false)
+    const [loading,setLoading]=useState(false)
 
+    useEffect(()=>{
+        if(user.email.length>0 && user.password.length >0){
+            setButtonDisabled(false)
+        }else{
+            setButtonDisabled(true)
+        }
+    },[user])
     const onLogin=async ()=>{
-
+        try {
+            setLoading(true);
+            const response=await axios.post("api/users/login",user);
+            toast.success('Login Success')
+            router.push('/profile')
+        } catch (error:any) {
+            console.log("Login Failed",error.message)
+            toast.error(error.message)
+        }finally{
+            setLoading(false);
+        }
     }
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
