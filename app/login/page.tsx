8ebd9209fb02fc86eb496997005836e54c7d4cbe
level237 <Bramslevel129@gmail.com>
2024-06-29 01:32:40 +0100

    middleware in action

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -37,7 +37,7 @@ export default function LoginPage(){
     }
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
-        <h1 className="text-center text-white text-2xl">Login</h1>
+        <h1 className="text-center text-white text-2xl">{!loading ? "Signup" : "Processing..."}</h1>
         <hr />
         <label htmlFor="email">email</label>
         <input
@@ -52,7 +52,7 @@ export default function LoginPage(){
      
         <label htmlFor="password">password</label>
         <input
-        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        className="p-2 border border-gray-300 text-black rounded-lg mb-4 focus:outline-none focus:border-gray-600"
          id="password"
           type="password"
           value={user.password}
